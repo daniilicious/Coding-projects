b23563675ce08dd42bcbddf1555851ef81b0bff8
daniC <dc.calderon91@gmail.com>
2024-07-16 21:06:01 -0500

    if, elif, else statements added and variable

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,15 @@
+import random
+choices = ["rock", "paper", "scissors"]
+computer_choice = random.choice(choices)
 print("Let's play rock, paper, scissors")
 
 player_choice = input(f"What do you choose? ").lower()
+print(f"Computer chose: " +computer_choice)
+
+if (player_choice == "rock" and computer_choice == "scissors") or (player_choice == "scissors" and computer_choice == "paper") or (player_choice == "paper" and computer_choice == "rock"):
+    winner = "Player"
+elif player_choice == computer_choice:
+    winner = "Tie"
+else:
+    winner = "Computer"
+print(f"The winner of the game is " + winner)
