60c6a56d022057e72942c414051a26900b7253b4
Daniela Calderon <dc.calderon91@gmail.com>
2024-07-18 01:00:49 -0500

    To_Do_List.py

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,4 +1,3 @@
-new-branch
 todo_list = []
 
 while True:
@@ -28,14 +27,3 @@ while True:
    elif choice == "3":
     print("Quitting program")
     break
-=======
-todo_list = ["groceries", "study"]
-
-if not todo_list:
-    print("Your ToDo list is empty")
-else:
-    index = 1
-    for task in todo_list:
-        print(f"{index}.{task}")
-        index += 1   
-main
