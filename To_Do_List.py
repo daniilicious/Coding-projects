03ecce463d8419132d55a4413dd4c0da6d68339e
daniC <dc.calderon91@gmail.com>
2024-07-17 23:14:22 -0500

    iteration and adding tasks

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,9 +1,13 @@
 todo_list = ["groceries", "study"]
+new_task = input("Enter the task:")
+
+todo_list.append(new_task)
+print(f"Task {new_task} added successfully")
 
 if not todo_list:
     print("Your ToDo list is empty")
 else:
     index = 1
     for task in todo_list:
-        print(f"{index}.{task}")
+        print(f"{index}. {task}")
         index += 1   
