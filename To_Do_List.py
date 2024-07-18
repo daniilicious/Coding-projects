1971865806d21382c21335407ed8626d6a7c64c0
daniC <dc.calderon91@gmail.com>
2024-07-18 00:20:19 -0500

    while loop and menu

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,13 +1,21 @@
 todo_list = ["groceries", "study"]
-new_task = input("Enter the task:")
 
-todo_list.append(new_task)
-print(f"Task {new_task} added successfully")
+while True:
+   new_task = input("Enter the task:")
+   todo_list.append(new_task)
+   print(f"Task {new_task} added successfully")
 
-if not todo_list:
-    print("Your ToDo list is empty")
-else:
+   if not todo_list:
+      print("Your ToDo list is empty")
+   else:
     index = 1
     for task in todo_list:
         print(f"{index}. {task}")
-        index += 1   
+        index += 1 
+   print("Options:")
+   print("1) Add a task")
+   print("2) Remove a task")
+   print("3) Quit the program")
+
+   choice = input("Choose a number from the menu: ")
+   break
