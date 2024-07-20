d01fe7de3eb009b892ea12464db3018fab68194a
daniC <dc.calderon91@gmail.com>
2024-07-19 22:55:06 -0500

    functions select and display

--- Food_Order_System.py
+++ Food_Order_System.py
@@ -1,8 +1,15 @@
-italian_food = ["Spaghetti Amatriciana", "Penne Arrabiata", "Fettuccine Carbonara", "Lasagna", "Pizza Margherita", "Risotto" ]
+italian_food = ["Spaghetti Amatriciana", "Penne Arrabiata", "Fettuccine Carbonara", "Lasagna", "Pizza Margherita", "Risotto" ] #food menu
 
-def find_meal (name, menu):
+def find_meal (name, menu): #checks if meal exist in menu
   if name in menu:
     return name
   else:
     return None
- 
+
+def select_meal (name): #meal choosen
+  return find_meal(name, italian_food) 
+
+def display_available_meals(): #shows food available
+  print("Available Italian Meals:")
+  for meal in italian_food:
+    print(meal)
