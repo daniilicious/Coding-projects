691b774c1cd454821e56f192bec0bd25adf8b67b
daniC <dc.calderon91@gmail.com>
2024-07-21 23:25:38 -0500

    added user decision and drawing

--- Draw_card.py
+++ Draw_card.py
@@ -8,16 +8,31 @@ def create_deck(): #function to reuse the deck
     random.shuffle(deck)
     return deck
 
-def draw_card(deck): #function to draw a card from the deck
-    hand = [deck[-1]]
-    deck.pop()
+def draw_card(deck, number_cards): #function to draw n cards from the deck
+    hand = deck[:number_cards]
+    del deck[:number_cards]
     return hand, deck
 
 deck = create_deck()
 
+def show_card(card): #draws cards one by one
+    space = " "
+    if len(card[1]) == 2:
+        space = ""
+    print (f"""
+    +---------+
+    |{card[1]}       {space}|
+    |         |
+    |    {card[0]}    |
+    |         |
+    |{space}       {card[1]}|
+    +---------+
+    """)  
+
 while len(deck) > 0: #loop to draw cards until deck is empty
-    input("Press enter to draw the next card") #waits for user to draw a card
-    hand, deck = draw_card(deck)
-    print(f"Your card is {hand[0]}")
+    num_cards = int(input("How many cards do you want to draw? ")) #user decides how many cards to draw
+    hand, deck = draw_card(deck,num_cards)
+    for card in hand:
+        show_card(card)
 
 print("We are out of cards")
