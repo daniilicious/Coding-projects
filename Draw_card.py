110d90c68a73d5e8e3fb363fcef33a739786d823
daniC <dc.calderon91@gmail.com>
2024-07-21 22:36:25 -0500

    function and loop to generate cards

--- Draw_card.py
+++ Draw_card.py
@@ -1,9 +1,23 @@
 #Draw a card project
 import random
-suits = ["♥", "♦", "♣", "♠"]
-ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-deck = [(suit, rank) for suit in suits for rank in ranks]
+def create_deck(): #function to reuse the deck
+    suits = ["♥", "♦", "♣", "♠"] #4 suits
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] #13 ranks
+    deck = [(suit, rank) for suit in suits for rank in ranks] #card creation
+    random.shuffle(deck)
+    return deck
 
-random.shuffle(deck)
+def draw_card(deck): #function to draw a card from the deck
+    hand = [deck[-1]]
+    deck.pop()
+    return hand, deck
 
+deck = create_deck()
+
+while len(deck) > 0: #loop to draw cards until deck is empty
+    input("Press enter to draw the next card") #waits for user to draw a card
+    hand, deck = draw_card(deck)
+    print(f"Your card is {hand[0]}")
+
+print("We are out of cards")
