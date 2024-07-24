2bd69583f0ccb731a36f9e145bd4a67ed14f9767
daniC <dc.calderon91@gmail.com>
2024-07-23 19:18:07 -0500

    if block

--- Star_wars_API.py
+++ Star_wars_API.py
@@ -1,7 +1,9 @@
 import requests
+
 option = "people"
 url = f"https://swapi.dev/api/{option}/"
-try: 
+
+try: #handles http errors
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
@@ -9,3 +11,9 @@ try:
     print(f"Successfully retieved {len(result)} entities")
 except requests.HTTPError as e:
     print(f"Failed to retrieve, error: {e}")
+
+if result: #displays star wars characters
+    for person in result:
+        print(person["name"])
+else:
+    print("Unable to download data")
