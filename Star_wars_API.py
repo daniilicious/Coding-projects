08985ced755d5b29ac56910262d367c89c0c9893
daniC <dc.calderon91@gmail.com>
2024-07-23 19:11:21 -0500

    added try and if blocks

--- Star_wars_API.py
+++ Star_wars_API.py
@@ -1,8 +1,11 @@
 import requests
 option = "people"
 url = f"https://swapi.dev/api/{option}/"
-response = requests.get(url)
-response.raise_for_status()
-data = response.json()
-result = data["results"]
-print(f"Successfully retieved {len(result)} entities")
+try: 
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    result = data["results"]
+    print(f"Successfully retieved {len(result)} entities")
+except requests.HTTPError as e:
+    print(f"Failed to retrieve, error: {e}")
