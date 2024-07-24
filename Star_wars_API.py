34a9eb4d4ff1c040d04c110c2fc9937a0be586cc
daniC <dc.calderon91@gmail.com>
2024-07-23 20:33:45 -0500

    new function, while loop..

--- Star_wars_API.py
+++ Star_wars_API.py
@@ -1,19 +1,27 @@
 import requests
 
-option = "people"
-url = f"https://swapi.dev/api/{option}/"
+def fetch_data(option, count): #let's user decide which data is pulled
+    
+    url = f"https://swapi.dev/api/{option}/"
+    result = []
 
-try: #handles http errors
-    response = requests.get(url)
-    response.raise_for_status()
-    data = response.json()
-    result = data["results"]
-    print(f"Successfully retieved {len(result)} entities")
-except requests.HTTPError as e:
-    print(f"Failed to retrieve, error: {e}")
+    while len(result) < count:
+        try: #handles http errors
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            result.extend(data["results"]) #extends result list
+            print(f"Successfully retieved {len(result)} entities")
+            url = data["next"] #next page of data
+        except requests.HTTPError as e:
+            print(f"Failed to retrieve, error: {e}")
+        if not url:
+            break
 
-if result: #displays star wars characters
-    for person in result:
-        print(person["name"])
-else:
-    print("Unable to download data")
+    if result: #displays star wars characters
+        for person in result[:count]:
+            print(person["name"])
+        else:
+            print("Unable to download data")
+fetch_data("people", 10)
+ 
