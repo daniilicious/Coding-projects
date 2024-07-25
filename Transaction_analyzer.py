f40879289cfc783df83aa334e9bbae1ddc233dcc
daniC <dc.calderon91@gmail.com>
2024-07-25 15:02:50 -0500

    First commit transaction analysis

---
+++ Transaction_analyzer.py
@@ -0,0 +1,21 @@
+data = [(749.17, "Investment Return"),
+  (-11.54, "Utilities"),
+  (-247.58, "Online Shopping"),
+  (981.17, "Investment Return"),
+  (-410.65, "Rent"),
+  (310.60, "Rent"),
+  (563.70, "Gift"),
+  (220.79, "Salary"),
+  (-49.85, "Car Maintenance"),
+  (308.49, "Salary"),
+  (-205.55, "Car Maintenance"),
+  (870.64, "Salary"),
+  (-881.51, "Utilities"),
+  (518.14, "Salary"),
+  (-264.66, "Groceries")]
+
+def print_transactions(transactions): #print transactions with format
+    for transaction in transactions:
+        amount, statement = transaction
+        print(f"${amount} - {statement}")
+print_transactions(data)
