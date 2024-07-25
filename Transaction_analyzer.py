93040d13f6e6a04e35ba88e9f5f2b5461d55e345
daniC <dc.calderon91@gmail.com>
2024-07-25 17:27:33 -0500

    user interaction

--- Transaction_analyzer.py
+++ Transaction_analyzer.py
@@ -27,7 +27,7 @@ def print_summary(transactions): #function to summarize transactions
     withdrawals = [transaction[0] for transaction in transactions if transaction[0] < 0]
     total_withdrawn = sum(withdrawals)
     print(f"${total_withdrawn} withdrawn")
-    balance = total_deposits + total_withdrawn
+    balance = format(total_deposits + total_withdrawn, ".2f")
     print(f"Current balance: ${balance}")
 
 
@@ -39,13 +39,26 @@ def analyze_transactions(transactions): #function to analyze transactions
     print(f"Largest deposit: {largest_deposit}")
     deposits = [transaction[0] for transaction in transactions if transaction[0] >= 0]
     total_deposit = sum(deposits)
-    average_deposit = total_deposit / len(deposits) if deposits else 0
+    average_deposit = format(total_deposit / len(deposits) if deposits else 0, ".2f")
     print(f"Average deposit: ${average_deposit}")
     withdrawals = [transaction[0] for transaction in transactions if transaction[0] < 0]
     total_withdrawals = sum(withdrawals)
-    average_withdrawal = total_withdrawals / len(withdrawals) if withdrawals else 0
+    average_withdrawal = format(total_withdrawals / len(withdrawals) if withdrawals else 0, ".2f")
     print(f"Average withdrawal: ${average_withdrawal}")
 
-
+print("These are the options for the transactions:")
+while True:
+    print("print")
+    print("analyze")
+    print("stop")
+    choice = input("Enter your choice: ")
+    if choice == "print":
+        print_summary(data)
+    elif choice == "analyze":
+        analyze_transactions(data)
+    elif choice == "stop":
+        break
+    else:
+        print("Invalid choice")
 
 
