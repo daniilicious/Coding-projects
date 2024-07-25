d7941059289cd0465337af6f49e1f5c31c8d52df
daniC <dc.calderon91@gmail.com>
2024-07-25 18:34:34 -0500

    variables and display loop

--- Book_library.py
+++ Book_library.py
@@ -10,9 +10,17 @@ class Book:
             return True
         else:
             return False
-    
     def return_book(self): #method for the property of books
         self.available = True
-
     def display_info(self): #method for the property of books
         print(f"Book is {self.title}, written by {self.author}, and is available: {'Yes' if self.available else 'No'}")
+
+#Class variables
+book1 = Book("The Hitchhikers Guide to the Galaxy", "Douglas Adams")
+book2 = Book("Malinche", "Laura Esquivel")
+book3 = Book("One Day", "David Nicholls")
+#Class list
+books = [book1, book2, book3]
+
+for book in books: #loop to display book info
+    book.display_info()
