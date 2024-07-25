ee968884183111871cf2c3b6ca2a2c11064553df
daniC <dc.calderon91@gmail.com>
2024-07-25 18:09:09 -0500

    class methods added

--- Book_library.py
+++ Book_library.py
@@ -1,5 +1,18 @@
-"""This is a library system that keeps track of books"""
+"""This is a library system that keeps track of books using OOP"""
 class Book:
     def __init__(self, title, author):
-        self.title = title
-        self.author = author
+        self.title = title #properties
+        self.author = author #properties
+        self.available = True #properties
+    def checkout(self): #method for the property of books
+        if self.available:
+            self.available = False
+            return True
+        else:
+            return False
+    
+    def return_book(self): #method for the property of books
+        self.available = True
+
+    def display_info(self): #method for the property of books
+        print(f"Book is {self.title}, written by {self.author}, and is available: {'Yes' if self.available else 'No'}")
